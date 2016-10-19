18a8086c8051fe33de54c63a525adee701039d14
Roman Shevchuk <rommssh@gmail.com>
2016-10-19 19:52:40 +0300

    Try Catch added

--- Load-ItemGroups-From-Project.ps1
+++ Load-ItemGroups-From-Project.ps1
@@ -3,20 +3,25 @@ Get-ChildItem "./" -Filter *.sfproj |
 		$ProjectFileName = $_.FullName | Resolve-Path -Relative
 		$ItemGroupsFileName = $ProjectFileName + '.ItemGroups'
 
-		[XML]$ProjectDocument = Get-Content $ProjectFileName
-		$ItemGroupsDocument = New-Object System.XML.XMLDocument
-		
-		$ItemGroupsDocument.InsertBefore($ItemGroupsDocument.CreateXmlDeclaration("1.0", "utf-8", $null), $ItemGroupsDocument.DocumentElement) | Out-Null
-		$ItemGroupsDocument.appendChild($ItemGroupsDocument.createElement('ItemGroups', $ProjectDocument.DocumentElement.NamespaceURI)) | Out-Null
+		Try {
+			[XML]$ProjectDocument = Get-Content $ProjectFileName
+			$ItemGroupsDocument = New-Object System.XML.XMLDocument
 
-		$ItemGroups = $ProjectDocument.Project.ItemGroup;
-		$ItemGroups | foreach { 
-			$ItemGroupsDocument.DocumentElement.AppendChild($ItemGroupsDocument.ImportNode($_, $true)) | Out-Null
+			$ItemGroupsDocument.InsertBefore($ItemGroupsDocument.CreateXmlDeclaration("1.0", "utf-8", $null), $ItemGroupsDocument.DocumentElement) | Out-Null
+			$ItemGroupsDocument.appendChild($ItemGroupsDocument.createElement('ItemGroups', $ProjectDocument.DocumentElement.NamespaceURI)) | Out-Null
+
+			$ItemGroups = $ProjectDocument.Project.ItemGroup;
+			$ItemGroups | foreach { 
+				$ItemGroupsDocument.DocumentElement.AppendChild($ItemGroupsDocument.ImportNode($_, $true)) | Out-Null
+			}
+
+			$ItemGroupsDocument.Save((Resolve-Path $ItemGroupsFileName))
+
+			Write-Host $ProjectFileName -NoNewline 
+			Write-Host " --> " -foregroundcolor "green" -NoNewline 
+			Write-Host $ItemGroupsFileName
+		} Catch {
+			Write-Host $_.Exception.Message -foregroundcolor "red"
+			Write-Host $_.Exception.ItemName -foregroundcolor "red"
 		}
-		
-		$ItemGroupsDocument.Save((Resolve-Path $ItemGroupsFileName))
-		
-		Write-Host $ProjectFileName -NoNewline 
-		Write-Host " --> " -foregroundcolor "green" -NoNewline 
-		Write-Host $ItemGroupsFileName
 	}
--- Load-ItemGroups-To-Project.ps1
+++ Load-ItemGroups-To-Project.ps1
@@ -2,22 +2,27 @@ Get-ChildItem "./" -Filter *.sfproj.ItemGroups |
 	Foreach-Object {
 		$ItemGroupsFileName = $_.FullName | Resolve-Path -Relative
 		$ProjectFileName = $ItemGroupsFileName -replace "\.ItemGroups$", ""
-		
-		[XML]$ItemGroupsDocument = Get-Content $ItemGroupsFileName
-		[XML]$ProjectDocument = Get-Content $ProjectFileName
-		
-		$ProjectDocument.Project.ItemGroup | foreach {
-			$_.ParentNode.RemoveChild($_) | Out-Null
-		}
 
-		$ItemGroups = $ItemGroupsDocument.ItemGroups.ItemGroup
-		$ItemGroups | foreach { 
-			$ProjectDocument.DocumentElement.AppendChild($ProjectDocument.ImportNode($_, $true)) | Out-Null
+		Try {
+			[XML]$ItemGroupsDocument = Get-Content $ItemGroupsFileName
+			[XML]$ProjectDocument = Get-Content $ProjectFileName
+
+			$ProjectDocument.Project.ItemGroup | foreach {
+				$_.ParentNode.RemoveChild($_) | Out-Null
+			}
+
+			$ItemGroups = $ItemGroupsDocument.ItemGroups.ItemGroup
+			$ItemGroups | foreach { 
+				$ProjectDocument.DocumentElement.AppendChild($ProjectDocument.ImportNode($_, $true)) | Out-Null
+			}
+
+			$ProjectDocument.Save((Resolve-Path $ProjectFileName))
+
+			Write-Host $ProjectFileName -NoNewline 
+			Write-Host " <-- " -foregroundcolor "green" -NoNewline 
+			Write-Host $ItemGroupsFileName
+		} Catch {
+			Write-Host $_.Exception.Message -foregroundcolor "red"
+			Write-Host $_.Exception.ItemName -foregroundcolor "red"
 		}
-		
-		$ProjectDocument.Save((Resolve-Path $ProjectFileName))
-		
-		Write-Host $ProjectFileName -NoNewline 
-		Write-Host " <-- " -foregroundcolor "green" -NoNewline 
-		Write-Host $ItemGroupsFileName
 	}
